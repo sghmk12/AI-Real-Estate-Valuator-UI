405ce42d54f0ddd77fd3cc06a60470e3d939add2
pbeqo <patbeqo@gmail.com>
2021-11-22 11:01:08 -0800

    Small touchups

--- src/components/settings-page/settings-page.tsx
+++ src/components/settings-page/settings-page.tsx
@@ -5,88 +5,91 @@ import { FormModal } from "../form-modal";
 import { ErrorAlert } from "../alerts";
 import { Error } from "../../types";
 import { options } from "./static";
-import defaultSettings from "./static/default-settings.json"
+import defaultSettings from "./static/default-settings.json";
 
-const STORAGE_KEY = "APP_SETTINGS"
+const STORAGE_KEY = "APP_SETTINGS";
 
 interface SettingsProps {
-    modalOpen: boolean;
-    handleModalClose: () => void;
-    onSetSettings?: (settings: any) => void;
+  modalOpen: boolean;
+  handleModalClose: () => void;
+  onSetSettings?: (settings: any) => void;
 }
 
 const SettingsPage: React.FC<SettingsProps> = (props) => {
-    const { modalOpen, handleModalClose, onSetSettings } = props;
-    const [loading, setLoading] = useState<boolean>(false);
-    const [settings, setSettings] = useState<any>(defaultSettings);
-    const [error, setError] = useState<Error>();
+  const { modalOpen, handleModalClose, onSetSettings } = props;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [settings, setSettings] = useState<any>(defaultSettings);
+  const [error, setError] = useState<Error>();
 
-    useEffect(() =>{
-        try{
-            const data = localStorage.getItem(STORAGE_KEY);
-            if(data){
-                const parsedData = JSON.parse(data); 
-                setSettings(parsedData);
-                if(onSetSettings) onSetSettings(parsedData)
-            }
-        }catch(e){
-            setError({msg: "Error retreiving previous settings"});
-        }
-    },[]);
+  useEffect(() => {
+    try {
+      const data = localStorage.getItem(STORAGE_KEY);
+      if (data) {
+        const parsedData = JSON.parse(data);
+        setSettings(parsedData);
+        if (onSetSettings) onSetSettings(parsedData);
+      }
+    } catch (e) {
+      setError({ msg: "Error retreiving previous settings" });
+    }
+  }, [onSetSettings, setSettings]);
 
-    const handleSaveSettings = useCallback(
-        async (event: any) => {
-            event.preventDefault();
-            setLoading(true);
-            try{
-                localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
-            } catch (e) {
-                setError({msg:"Sorry, we could not save your settings"});
-            }
-            setLoading(false);
-            handleModalClose();
-        },
-        []
-    );
+  const handleSaveSettings = useCallback(async (event: any) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
+    } catch (e) {
+      setError({ msg: "Sorry, we could not save your settings" });
+    }
+    setLoading(false);
+    handleModalClose();
+  }, [setLoading, handleModalClose, settings]);
 
-    const handleSettingsClose = useCallback(() => {
-        handleModalClose();
-    }, []);
+  const handleSettingsClose = useCallback(() => {
+    handleModalClose();
+  }, [handleModalClose]);
 
-    const updateSettings = useCallback((event) => {
-        setSettings((prev : any) => {
-            return prev? {
-                 ...prev,
-                 [event.target.id]: event.target.value
-            } : {
-                ...defaultSettings,
-                [event.target.id]: event.target.value
-            };
-        })
-    }, []);
+  const updateSettings = useCallback((event) => {
+    setSettings((prev: any) => {
+      return prev
+        ? {
+            ...prev,
+            [event.target.id]: event.target.value,
+          }
+        : {
+            ...defaultSettings,
+            [event.target.id]: event.target.value,
+          };
+    });
+  }, [setSettings]);
 
-    return (
-        <FormModal
-            isOpen={modalOpen}
-            onClose={handleSettingsClose}
-            headerText="Settings"
-            saveText="Save"
-            cancelText="Cancel"
-            onSave={handleSaveSettings}
-            disabled={false}
-            loading={loading}
-        >
-            {error && <ErrorAlert msg={error.msg || "There was an error getting your settings!"} />}
-            {options.map(({ id, label, options }) => (
-            <FormControl marginTop={4} id={id} isRequired>
-                <FormLabel>{label}</FormLabel>
-                <Select value={settings[id]} onChange={updateSettings}>
-                {options}
-                </Select>
-            </FormControl>
-            ))}
-        </FormModal>
-    );
+  return (
+    <FormModal
+      isOpen={modalOpen}
+      onClose={handleSettingsClose}
+      headerText="Settings"
+      saveText="Save"
+      cancelText="Cancel"
+      onSave={handleSaveSettings}
+      disabled={false}
+      loading={loading}
+    >
+      {error && (
+        <ErrorAlert
+          msg={error.msg || "There was an error getting your settings!"}
+        />
+      )}
+      {options.map(({ id, label, options }) => (
+        <FormControl marginTop={4} id={id}>
+          <FormLabel>{label}</FormLabel>
+          <Select value={settings[id]} onChange={updateSettings}>
+            {options}
+          </Select>
+        </FormControl>
+      ))}
+    </FormModal>
+  );
 };
 
 const MemoSettings = React.memo(SettingsPage);
